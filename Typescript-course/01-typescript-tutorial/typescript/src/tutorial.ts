7982b36251e66723815e187695b5485432e462a4
Bruno Costa <costabruno.dev@gmail.com>
2024-05-04 16:46:18 -0300

    typescript functions

--- Typescript-course/01-typescript-tutorial/typescript/src/tutorial.ts
+++ Typescript-course/01-typescript-tutorial/typescript/src/tutorial.ts
@@ -1,5 +1,16 @@
-console.log('tutorial typescript');
+function processData(
+  input: string | number,
+  config: { reverse: boolean} = {reverse: false}
+) {
+    if (typeof input === 'number'){
+        return input*input
+    } else if (config.reverse === true && typeof input === 'string') {
+        return input.toString().split('').reverse().join('').toUpperCase();
+    } else {
+        return input.toUpperCase()
+    } 
+}
 
-
+console.log(processData('100'));
 
 
