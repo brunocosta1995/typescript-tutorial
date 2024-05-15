c2666a7ccec9788b18b5cf9388841853d5af5866
Bruno Costa <costabruno.dev@gmail.com>
2024-05-14 21:49:17 -0300

    Type Guards - Desafios

--- Typescript-course/01-typescript-tutorial/typescript/src/tutorial.ts
+++ Typescript-course/01-typescript-tutorial/typescript/src/tutorial.ts
@@ -1,180 +1,130 @@
-// let person: [string, number] = ['john', 24];
+type ValueType = string | number | boolean;
 
-// let data: readonly [number, number, number] = [29, 6, 1995];
+let value: ValueType;
+const random = Math.random();
+value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
 
-// let opcional: [string, number?] = ['bruno'];
-
-// const returnPerson = (): [string, number] => {
-//   return person;
-// }
-
-// console.log(returnPerson()[1]);
-
-// enum ServerResponse{
-//   success = 'success',
-//   error = 'error'
-// }
-
-// Object.values(ServerResponse).forEach((values) => {
-//   if (typeof values === 'number') {
-//     console.log(values);
-
-//   }
-
-// })
-
-// console.log(ServerResponse);
-
-// interface ServerStatus {
-//   result: ServerResponse,
-//   data: string[]
-// }
-
-// function getServerResponse(): ServerStatus {
-//   return {
-//     result: ServerResponse.success,
-//     data: ['firstData', 'secondData']
-//   }
-// }
-
-// console.log(getServerResponse());
-
-// enum UserRole {
-//   Admin,
-//   Manager,
-//   Employee
-// }
-
-// type User = {
-//   id: number;
-//   name: string;
-//   role: UserRole;
-//   contact: [email: string, phone: string]
-// }
-
-// function createUser(user: User): User {
-//   return user;
-// }
-
-// const userBruno = createUser(
-//   {
-//     id: 1,
-//     name: 'bruno',
-//     role: UserRole.Employee,
-//     contact: ['bruno@bruno.com', '1999523288']
-//   }
-// )
-//  console.log(userBruno);
-
-// let strName: string = 'Bruno';
-// let strLength: number = (<string>strName).length;
-
-// type Bird = {
-//   name: string
-// }
-
-// let birdString = '{"name": "Eagle"}';
-// let dogString = '{"breed": "Poodle"}';
-
-// let newBird = JSON.parse(birdString);
-// let newDog = JSON.parse(dogString);
-
-// let bird = newBird as Bird;
-// let dog = newDog as Bird;
-
-// console.log(bird.name);
-// console.log(dog.name);
-
-// let unknownValue: unknown;
-
-// unknownValue = "bruno";
-// unknownValue = 44.236554;
-// unknownValue = true;
-
-// if (typeof unknownValue === "number") {
-//   let newValue = unknownValue.toFixed(2);
-//   unknownValue = newValue;
-// }
-
-// function runCode() {
-//   let value = Math.random();
-//   if (value < 0.5) {
-//     throw new Error("Somenthing went wrong");
-//   } else {
-//     throw "new error";
-//   }
-// }
-
-// try {
-//   runCode();
-// } catch (error) {
-//   if (error instanceof Error) {
-//     console.log(error.message);
-//   } else {
-//     console.log(error);
-//     console.log("erro ocorrido");
-//   }
-// }
-
-
-// let value: never;
-
-// type Theme = 'dark' | 'light';
-
-// function checkTheme(theme: Theme) {
-//   if (theme === 'dark') {
-//     console.log('this is dark');
-//     return;
-//   } else if (theme === 'light') {
-//     console.log('this is light');
-//     return;
-//   }
-//   theme
-// }
-
-// enum Color {
-//   Red,
-//   Blue,
-//   // Green
-// }
-
-// function getColor(color: Color) {
-//   switch (color) {
-//     case Color.Red:
-//       return 'red';
-//     case Color.Blue:
-//       return 'blue';
-//     default:
-//       let inesperado: never = color;
-//       throw new Error(`Valor de cor não experado: ${inesperado}`);
-//   }
-// }
-
-
-// console.log(getColor(Color.Red));
-// console.log(getColor(Color.Blue));
-// console.log(getColor(Color.Green));
-
-
-
-// const susan = 'susan';
-
-// const name = 'name';
-
-import newStudent, {sayHello, person, type Student} from "./actions";
-
-sayHello('Typescript');
-console.log(newStudent);
-console.log(person);
-
-const anotherStudent: Student = {
-  name: 'Bruno',
-  age: 28
+function checkValue(value: ValueType) {
+  if (typeof value === 'string') {
+    console.log(value.toLowerCase());
+    return;
+  }
+  if (typeof value === 'number') {
+    console.log(value.toFixed(2));
+    return;    
+  }
+  if (typeof value === 'boolean') {
+    console.log(`boolean: ${value}`);
+    return;
+  }
 }
 
-console.log(anotherStudent);
+checkValue(value);
 
-import {example} from './example.js';
+type Dog = {type: 'dog'; name: string; bark: ()=> void}
+type Cat = {type: 'cat'; name: string; meow: ()=> void}
 
-console.log(example);
+type Animal = Dog | Cat;
 
+function makeSound(animal: Animal) {
+  if (animal.type === 'dog') {
+    animal.bark();
+  } else {
+    animal.meow();
+  }
+}
+
+const newDog: Dog = {
+  type: 'dog',
+  name: 'bruno',
+  bark() {
+    console.log('awaw');
+  },
+}
+const newCat: Cat = {
+  type: 'cat',
+  name: 'chanel',
+  meow() {
+   console.log('miau');
+    
+  }
+}
+
+// makeSound(newDog);
+// makeSound(newCat);
+
+function newSound(animal: Animal) {
+  if ('bark' in animal) {
+    animal.bark();
+  } else {
+    animal.meow();
+  }
+}
+
+newSound(newDog);
+newSound(newCat);
+
+function printLength(str: string | null | undefined) {
+  if (str) {
+    console.log(str.length);    
+  } else {
+    console.log(`${str} - No string provided`);
+  }
+}
+
+printLength('Bruno');
+printLength(undefined);
+
+try {
+  throw new Error("Erro ocorrido");
+} catch (error) {
+  if (error instanceof Error){
+    console.log('Erro do Error object: ' + error.message);
+  } else {
+    console.log('Erro desconhecido');
+  }
+}
+
+function checkInput(input: Date | string) {
+  if (input instanceof Date) {
+    return input.getFullYear().toString();
+  } else {
+    return input;
+  }
+}
+
+
+const today = new Date();
+
+console.log(checkInput(today));
+console.log(checkInput('bruno'));
+
+type Student = {
+  name: string;
+  study(): void
+}
+
+type User = {
+  name: string;
+  login(): void
+}
+
+type Person = Student | User;
+
+const randomPerson = (): Person => {
+  return Math.random() > 0.5 
+  ? {name: 'john', study() {console.log('studying');} }
+  : {name: 'mary', study() {console.log('logging in');}}
+}
+
+const person = randomPerson();
+
+function isStudent(person: Person): person is Student {
+  return (person as Student).study !== undefined;
+}
+
+if (isStudent(person)) {
+  person.study();
+} else {
+  person.login();
+}
